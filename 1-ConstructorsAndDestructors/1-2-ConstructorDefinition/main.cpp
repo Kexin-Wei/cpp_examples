0b30cc3b41376f6253e0a15523c5e980440d29bf
Kexin-Wei <wkx611@outlook.com>
2024-11-01 14:19:38 +0800

    split examples

--- 1-ConstructorsAndDestructors/1-2-ConstructorDefinition/main.cpp
+++ 1-ConstructorsAndDestructors/1-2-ConstructorDefinition/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
-#include "default_constructor.h"
-
 struct S {
   int n;
 
-  explicit S(int); // constructor declaration
+  explicit S(int);  // constructor declaration
 
-  S() : n(7) {} // constructor definition:
+  S() : n(7) {}  // constructor definition:
   // ": n(7)" is the initializer list
   // ": n(7) {}" is the function body
 };
@@ -15,11 +13,9 @@ struct S {
 S::S(const int x) : n{x} {}
 // constructor definition: ": n{x}" is the initializer list
 
-
 int main() {
   S s;
   S s2(10);
 
-  std::cout << "Hello, World!" << std::endl;
   return 0;
 }
